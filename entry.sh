a9b5f4962355f8cf4836710f395f52ad3fbb3ada
Juan Cuzmar <juan.cuzmar.s@gmail.com>
2019-09-10 18:29:27 -0300

    phpmyadmin last version
    change the way how download phpmyadmin to avoid problems of override
    with the volume option in run

--- entry.sh
+++ entry.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+mkdir -p /usr/share/webapps/ && cd /usr/share/webapps/ && \
+    wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz > /dev/null 2>&1 && \
+    tar -xzvf phpMyAdmin-4.9.0.1-all-languages.tar.gz > /dev/null 2>&1 && \
+    mv phpMyAdmin-4.9.0.1-all-languages phpmyadmin && \
+    chmod -R 777 /usr/share/webapps/ && \
+    ln -s /usr/share/webapps/phpmyadmin/ /var/www/localhost/htdocs/phpmyadmin
+
 # start apache
 httpd
 
